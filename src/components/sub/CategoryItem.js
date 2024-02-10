8a9019d307c62a3b88d2e1e09d0f378331acbf03
hmaminulislam <mdaminulislamsahin14@gmail.com>
2024-02-10 23:35:52 +0600

    completed

--- src/components/sub/CategoryItem.js
+++ src/components/sub/CategoryItem.js
@@ -5,16 +5,20 @@ import DuaArrowIcon from "../../assets/images/duaarrow.svg";
 
 function CategoryItem({ catItem, data }) {
   const [open, setOpen] = useState(false);
+  const [openSub, setOpenSub] = useState(false)
+  const [subCatId, setSubCatId] = useState()
   const subItem = useRef();
+  const subCateItem = useRef();
   const { sub_category, dua } = data;
   const { cat_name_en } = catItem;
   const catItemId = catItem.id;
-  const filterSubCategory = sub_category.filter(
+  const SubCategory = sub_category.filter(
     (item) => catItemId === item.cat_id
   );
-  const filterDua = dua.filter((d) => catItemId === d.subcat_id);
-  console.log(filterDua);
+  const filterDua = dua.filter((d) => subCatId === d.subcat_id);
 
+console.log(filterDua)
+  // category open close 
   if (open) {
     subItem.current.style.display = "block";
   } else {
@@ -22,9 +26,26 @@ function CategoryItem({ catItem, data }) {
       subItem.current.style.display = "none";
     }
   }
+  // sub category open close 
+  // if (openSub) {
+  //   subCateItem.current.style.display = "block";
+  // } else {
+  //   if (subCateItem.current) {
+  //     subCateItem.current.style.display = "none";
+  //   }
+  // }
+  // handle category open close
   const handleCategory = () => {
     setOpen(!open);
   };
+  // handle sub category open close
+  const handleSubCategory = (item, e) => {
+    setSubCatId(item.subcat_id);
+    // setOpenSub(!openSub);
+    subCateItem.current.style.display ='block'
+    console.log(subCateItem.current);
+  };
+  // console.log(openSub)
   return (
     <div className="mt-4">
       <div
@@ -42,40 +63,47 @@ function CategoryItem({ catItem, data }) {
               ""
             )}
             <p className="text-gray-400 text-xs mt-1 xs:text-[11px]">
-              Subcategory: {filterSubCategory?.length}
+              Subcategory: {SubCategory?.length}
             </p>
           </div>
           <div className="flex flex-col items-center">
-            <p className="text-base xs:text-sm">15</p>
+            <p className="text-base xs:text-sm">{filterDua?.length}</p>
             <p className="text-gray-400 text-xs xs:text-[11px]">Duas</p>
           </div>
         </div>
       </div>
       <div ref={subItem} className="pl-10 pr-5 mt-4 hidden">
         <div className="flex flex-col gap-5 border-l-[2px] border-dotted border-[#1FA45B]">
-          {filterSubCategory?.map((item) => (
+          {SubCategory?.map((item) => (
             <>
               <div>
-                <div className="flex gap-4 items-start mb-[5px]">
+                <div
+                  onClick={(e) => handleSubCategory(item, e)}
+                  className="flex gap-4 items-start mb-[5px]"
+                >
                   <div className="w-[8px] h-[8px] bg-primary rounded-[50%] ml-[-4px] mt-[7px]"></div>
                   <p className="cursor-pointer font-semibold text-left text-sm">
                     {item.subcat_name_en}
                   </p>
                 </div>
-                {filterDua.map((dua) => (
-                  <div className="flex items-start w-[95%] ml-auto gap-2">
-                    <Image
-                      className="mt-[9px]"
-                      src={DuaArrowIcon}
-                      width={18}
-                      height={18}
-                      alt=""
-                    />
-                    <p className="my-3 text-[14px] text-left">
-                      {dua?.dua_name_en}
-                    </p>
-                  </div>
-                ))}
+                <div ref={subCateItem} className="" style={{ display: 'none'}}>
+                  {filterDua.map((dua, index) => (
+                    <div key={index}>
+                      <div className="flex items-start w-[95%] ml-auto gap-2">
+                        <Image
+                          className="mt-[9px]"
+                          src={DuaArrowIcon}
+                          width={18}
+                          height={18}
+                          alt=""
+                        />
+                        <p className="my-3 text-[14px] text-left">
+                          {dua?.dua_name_en}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
               </div>
             </>
           ))}
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home({data}) {
       </div>
       </div>
     </main>
-  );
+  )
 }
 
 
